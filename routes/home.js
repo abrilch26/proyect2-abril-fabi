395729d8945fa7fb6af5aa9f49ac515cce8b8cef
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-29 11:08:19 -0600

    refactoring

--- routes/home.js
+++ routes/home.js
@@ -1,24 +1,26 @@
 //1. IMPORTACIONES
 const express = require("express");
 const router = express.Router();
-const homeController = require("./../controllers/homeController");
+const {
+  home,
+  getSignup,
+  postSignup,
+  getLogin,
+  postLogin,
+} = require("./../controllers/homeController");
 
 //HOME
-router.get("/", homeController.home)
+router.get("/", home);
 
 //REGISTER
-router.get("/signup", homeController.getSignup)
-router.post("/signup", homeController.postSignup)
-
-
-//EXPORTACION
-module.exports = router
-router.get("/", homeController.home);
+router.get("/signup", getSignup);
+router.post("/signup", postSignup);
 
 //LOGIN
 //renderizar el form
-router.get("/login", homeController.getLogin);
+router.get("/login", getLogin);
 //mandar datos
-router.post("/login", homeController.postLogin);
+router.post("/login", postLogin);
 
+//EXPORTACION
 module.exports = router;
